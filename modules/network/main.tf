e711503bf36e69ac9fa0716e9b4667d4fad2721e
laxmi1912 <Klaxmipriya1912@gmail.com>
2024-11-19 21:47:48 +0530

    Added allow HTTP traffic to port 80 in NSG

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -48,5 +48,17 @@ resource "azurerm_network_security_group" "vm_sg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 80
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
